3bf1184602924391ed04c39a4cbeb70b98cf86c9
Maxwell <llewxam100@hotmail.com>
2013-10-15 09:36:41 -0300

    Criação da classe DefaltCotaParlamentar

---
+++ src/com/omm/dao/DefaultCotaParlamentarDao.java
@@ -0,0 +1,28 @@
+package com.omm.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import com.omm.model.CotaParlamentar;
+
+/**
+ * 
+ */
+public class DefaultCotaParlamentarDao extends DataAccessObject implements
+		CotaParlamentarDao {
+
+	public List<CotaParlamentar> listar(String id) {
+		EntityManager manager = getEntityManagerFactory().createEntityManager();
+
+		Query query = manager
+				.createQuery("SELECT c FROM CotaParlamentar c WHERE c.idParlamentar = :id"); // jpql
+		query.setParameter("id", Integer.parseInt(id));
+
+		List<CotaParlamentar> resultado = (List<CotaParlamentar>) query
+				.getResultList();
+		manager.close();
+		return resultado;
+	}
+}
