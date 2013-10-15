71e70ce69c527308ebe73f5cde4c701b6db2a093
Rudmar <rudmar_junior@hotmail.com>
2013-10-15 10:00:20 -0300

    Criação da classe ParlamentarServlet

--- src/com/omm/helper/JSONHelper.java
+++ src/com/omm/helper/JSONHelper.java
@@ -4,10 +4,7 @@ import com.google.gson.Gson;
 
 public abstract class JSONHelper {
 	public static String toJSON(Object param) {
-		// instanciar parser
 		Gson gson = new Gson();
-		
-		// transformar em texto formar json
 		return gson.toJson(param);
 	}
 }
