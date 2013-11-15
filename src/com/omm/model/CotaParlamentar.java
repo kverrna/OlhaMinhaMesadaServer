294041ac6e95f39f0b4dc38f19233fbe0f8341ef
Ramon +Arthur <kverrna@gmail.com>
2013-11-15 19:34:24 -0200

    Refatoracao classe Cota parlamentar

--- src/com/omm/model/CotaParlamentar.java
+++ src/com/omm/model/CotaParlamentar.java
@@ -6,73 +6,48 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Ceap") 
+@Table(name = "cota")
 public class CotaParlamentar {
-
-	@Id
-	@Column(name = "idCeap")
-	int id;
+		@Id
+	@Column(name="ID_COTA")
+	int cod;
 	
-	@Column(name="idParlametar")
+
+	@Column(name = "ID_PARLAMENTAR")
 	int idParlamentar;
 
-	@Column(name = "anoCeap")
+	@Column(name = "MES")
+	int mes;
+
+	@Column(name = "ANO")
 	int ano;
 
-	@Column(name = "numeroSubCota")
+	@Column(name = "NUM_SUBCOTA")
 	int numeroSubCota;
 
-	@Column
-	double valorJaneiro;
-
-	@Column
-	double valorFevereiro;
-
-	@Column
-	double valorMarco;
-
-	@Column
-	double valorAbril;
-
-	@Column
-	double valorMaio;
-
-	@Column
-	double valorJunho;
-
-	@Column
-	double valorJulho;
-
-	@Column
-	double valorAgosto;
-
-	@Column
-	double valorSetembro;
-
-	@Column
-	double valorOutubro;
-
-	@Column
-	double valorNovembro;
-
-	@Column
-	double valorDezembro;
-
-	@Column(name = "descricaoCeap")
+	@Column(name = "DESCRICAO")
 	String descricao;
 
-	@Column(name = "especificacaoCeap")
-	String especificacao;
-	
+	@Column(name = "VALOR")
+	double valor;
+
 	public CotaParlamentar() {
 	}
 
-	public int getId() {
-		return id;
+	public int getIdParlamentar() {
+		return idParlamentar;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setIdParlamentar(int idParlamentar) {
+		this.idParlamentar = idParlamentar;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
 	}
 
 	public int getAno() {
@@ -91,102 +66,6 @@ public class CotaParlamentar {
 		this.numeroSubCota = numeroSubCota;
 	}
 
-	public double getValorJaneiro() {
-		return valorJaneiro;
-	}
-
-	public void setValorJaneiro(double valorJaneiro) {
-		this.valorJaneiro = valorJaneiro;
-	}
-
-	public double getValorFevereiro() {
-		return valorFevereiro;
-	}
-
-	public void setValorFevereiro(double valorFevereiro) {
-		this.valorFevereiro = valorFevereiro;
-	}
-
-	public double getValorMarco() {
-		return valorMarco;
-	}
-
-	public void setValorMarco(double valorMarco) {
-		this.valorMarco = valorMarco;
-	}
-
-	public double getValorAbril() {
-		return valorAbril;
-	}
-
-	public void setValorAbril(double valorAbril) {
-		this.valorAbril = valorAbril;
-	}
-
-	public double getValorMaio() {
-		return valorMaio;
-	}
-
-	public void setValorMaio(double valorMaio) {
-		this.valorMaio = valorMaio;
-	}
-
-	public double getValorJunho() {
-		return valorJunho;
-	}
-
-	public void setValorJunho(double valorJunho) {
-		this.valorJunho = valorJunho;
-	}
-
-	public double getValorJulho() {
-		return valorJulho;
-	}
-
-	public void setValorJulho(double valorJulho) {
-		this.valorJulho = valorJulho;
-	}
-
-	public double getValorAgosto() {
-		return valorAgosto;
-	}
-
-	public void setValorAgosto(double valorAgosto) {
-		this.valorAgosto = valorAgosto;
-	}
-
-	public double getValorSetembro() {
-		return valorSetembro;
-	}
-
-	public void setValorSetembro(double valorSetembro) {
-		this.valorSetembro = valorSetembro;
-	}
-
-	public double getValorOutubro() {
-		return valorOutubro;
-	}
-
-	public void setValorOutubro(double valorOutubro) {
-		this.valorOutubro = valorOutubro;
-	}
-
-	public double getValorNovembro() {
-		return valorNovembro;
-	}
-
-	public void setValorNovembro(double valorNovembro) {
-		this.valorNovembro = valorNovembro;
-	}
-
-	public double getValorDezembro() {
-		return valorDezembro;
-	}
-
-	public void setValorDezembro(double valorDezembro) {
-		this.valorDezembro = valorDezembro;
-	}
-
 	public String getDescricao() {
 		return descricao;
 	}
@@ -195,20 +74,12 @@ public class CotaParlamentar {
 		this.descricao = descricao;
 	}
 
-	public String getEspecificacao() {
-		return especificacao;
+	public double getValor() {
+		return valor;
 	}
 
-	public void setEspecificacao(String especificacao) {
-		this.especificacao = especificacao;
+	public void setValor(double valor) {
+		this.valor = valor;
 	}
-	
-//	public void setIdParlamentar(int id) {
-//		this.idParlamentar = id;
-//	}
-	
-	//public int getIdParlmanetar() {
-	//	return this.idParlamentar;
-//	}
 
 }
--- src/com/omm/test/model/CotaParlamentarTest.java
+++ src/com/omm/test/model/CotaParlamentarTest.java
@@ -22,11 +22,7 @@ public class CotaParlamentarTest {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testSetId() {
-		cotaParlamentar.setId(1);
-		assertEquals(1, cotaParlamentar.getId());
-	}
+
 
 	@Test
 	public void testSetAno() {
@@ -44,73 +40,17 @@ public class CotaParlamentarTest {
 		assertEquals(1, cotaParlamentar.getNumeroSubCota());
 	}
 	
+	
 	@Test
-	public void testSetValorJaneiro() {
-		cotaParlamentar.setValorJaneiro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorJaneiro(), .01f);
+	public void testSetMes() {
+		cotaParlamentar.setMes(1);
+		assertEquals(1, cotaParlamentar.getMes());
 	}
 	
 	@Test
-	public void testSetValorFevereiro() {
-		cotaParlamentar.setValorFevereiro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorFevereiro(), .01f);
+	public void testSetValor() {
+		cotaParlamentar.setValor(1.0f);
+		assertEquals(1.0f, cotaParlamentar.getValor(),.01f);
 	}
 	
-	@Test
-	public void testSetValorMarco() {
-		cotaParlamentar.setValorMarco(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorMarco(), .01f);
-	}
-	
-	@Test
-	public void testSetValorAbril() {
-		cotaParlamentar.setValorAbril(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorAbril(), .01f);
-	}
-	
-	@Test
-	public void testSetValorMaio() {
-		cotaParlamentar.setValorMaio(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorMaio(), .01f);
-	}
-	@Test
-	public void testSetValorJunho() {
-		cotaParlamentar.setValorJunho(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorJunho(), .01f);
-	}
-	@Test
-	public void testSetValorJulho() {
-		cotaParlamentar.setValorJulho(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorJulho(), .01f);
-	}
-	
-	@Test
-	public void testSetValorAgosto() {
-		cotaParlamentar.setValorAgosto(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorAgosto(), .01f);
-	}
-	
-	@Test
-	public void testSetValorSetembro() {
-		cotaParlamentar.setValorSetembro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorSetembro(), .01f);
-	}
-	
-	@Test
-	public void testSetValorOutubro() {
-		cotaParlamentar.setValorOutubro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorOutubro(), .01f);
-	}
-	
-	@Test
-	public void testSetValorNovembro() {
-		cotaParlamentar.setValorNovembro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorNovembro(), .01f);
-	}
-	
-	@Test
-	public void testSetValorDezembro() {
-		cotaParlamentar.setValorDezembro(1.0f);
-		assertEquals(1.0f, cotaParlamentar.getValorDezembro(), .01f);
-	}
 }
